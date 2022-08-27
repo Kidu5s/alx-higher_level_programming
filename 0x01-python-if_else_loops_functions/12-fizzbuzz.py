1c802c77c1a19d82a4fde362fecc4eab6c28da16
Kidus Zelalem <zkidu5s@gmail.com>
2022-08-27 12:48:06 -0700

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if ((i % 5 == 0) & (i % 3 == 0)):
+            print("FizzBuzz", end=' ')
+        elif i % 3 == 0:
+            print("Fizz", end=' ')
+        elif ((i % 5 == 0) & (i != 100)):
+            print("Buzz", end=' ')
+        elif (i == 100):
+            print("Buzz")
+        else:
+            print(i, end=' ')
+            
