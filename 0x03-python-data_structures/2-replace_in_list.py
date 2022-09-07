5ec0ad003ebbe341cee7840f5031830d16a96eec
Kidus Zelalem <zkidu5s@gmail.com>
2022-09-07 14:30:02 -0700

    replace in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replac_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return(my_list)
+    else:
+        new_list = my_list.insert(idx, element)
+        return(new_list)
