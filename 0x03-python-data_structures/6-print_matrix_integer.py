6f31103bd5e1790632cc63f915a19b9360325f5b
Kidus Zelalem <zkidu5s@gmail.com>
2022-11-24 05:41:48 -0800

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for item in row:
             print("{:d}".format(item), end=' ')
-                if item != len(row):
-                    print(end='')
         print()
