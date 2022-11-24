f7f60245b7ba14ea878568d1935e6aebfed756a7
Kidus Zelalem <zkidu5s@gmail.com>
2022-11-24 07:26:17 -0800

    max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_value = my_list[0]
     if my_list == []:
         return None
     else:
+        max_value = my_list[0]
         for i in range(len(my_list)):
             if max_value > my_list[i]:
                 continue
